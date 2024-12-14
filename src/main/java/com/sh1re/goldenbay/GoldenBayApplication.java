e1eee2c00f0c75710a44e53618b6c1574b646829
sh1re-3201 <anugrahmapia@gmail.com>
2024-12-14 22:34:23 +0700

    Initial Commit

---
+++ src/main/java/com/sh1re/goldenbay/GoldenBayApplication.java
@@ -0,0 +1,13 @@
+package com.sh1re.goldenbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenBayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenBayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh1re/goldenbay/GoldenBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh1re.goldenbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenBayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
