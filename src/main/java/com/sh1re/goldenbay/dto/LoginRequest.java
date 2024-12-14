8dbc7342b7950e2cc6edd45a211b00aa352afff5
sh1re-3201 <anugrahmapia@gmail.com>
2024-12-14 23:20:19 +0700

    Feat: \\ Added the backend structure for the spring project: \\\\ Entities, Repositories, DTOs, JWT implementations, SecurityConfig.
    \\ Left to add:
    \\\\ Controllers, Service
    
    \\ Correction to the LoginRequest class and added more application.properties debug levels

--- src/main/java/com/sh1re/goldenbay/dto/LoginRequest.java
+++ src/main/java/com/sh1re/goldenbay/dto/LoginRequest.java
@@ -1,11 +1,23 @@
 package com.sh1re.goldenbay.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class LoginRequest {
     private String username;
     private String password;
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
