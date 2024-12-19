f778f90ea4fbc57f0f89c802c735249d07e582d7
sh1re-3201 <anugrahmapia@gmail.com>
2024-12-19 21:56:40 +0700

    Feat: \\ BLUEBERRY BUILD \\\\
    \\ Left to add:
    \\\\ Verifying login.html is correct(NEED FIX), Dashboard/landing page(NEED FIX), Each pages for the methods
    
    \\ Blueberry build:
    \\\\ Login still broken, seems like the username and password works but nothing else working. The dashboard cannot be accessed it returns code 500

---
+++ src/main/java/com/sh1re/goldenbay/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.sh1re.goldenbay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard"; // Maps to dashboard.html
+    }
+}
